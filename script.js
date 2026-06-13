bc45c2c7a28a9d8a5e29febc518c16a03abe0c5e
Joshuamichael <joshuamic811@gmail.com>
2026-06-13 01:16:25 +0100

    Initial portfolio upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const words = [
+    "Cybersecurity Enthusiast",
+    "DevSecOps Learner",
+    "Frontend Developer",
+    "Python Programmer"
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+
+const typing = document.getElementById("typing");
+
+function typeEffect() {
+
+    if(charIndex < words[wordIndex].length){
+        typing.textContent += words[wordIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(typeEffect,100);
+    }
+
+    else{
+        setTimeout(eraseEffect,1500);
+    }
+}
+
+function eraseEffect(){
+
+    if(charIndex > 0){
+        typing.textContent =
+        words[wordIndex].substring(0,charIndex-1);
+
+        charIndex--;
+        setTimeout(eraseEffect,50);
+    }
+
+    else{
+        wordIndex++;
+
+        if(wordIndex >= words.length){
+            wordIndex = 0;
+        }
+
+        setTimeout(typeEffect,300);
+    }
+}
+
+typeEffect();
